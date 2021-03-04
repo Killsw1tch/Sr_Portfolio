fcc998ad980a19e28b7a2f0458a80a34c37865e3
Hunter Wille <48933001+Killsw1tch@users.noreply.github.com>
2021-03-04 12:52:31 -0700

    Commit

--- JournalProject/flask-login-and-signup-master/app.py
+++ JournalProject/flask-login-and-signup-master/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, g
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField
@@ -23,6 +23,24 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(20))
     email = db.Column(db.String(30), unique=True)
     password = db.Column(db.String(80))
+    characters = db.relationship('Character', backref='user', lazy=True)
+
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    ancestry = db.Column(db.String(50), nullable=False)
+    archetype = db.Column(db.String(50), nullable=False)
+    system = db.Column(db.String(50), nullable=False)
+    entries = db.relationship('Entry', backref='character', lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(User.id))
+
+
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    entry = db.Column(db.Text(50000), nullable=False)
+    character_id = db.Column(db.Integer, db.ForeignKey(Character.id))
 
 
 @login_manager.user_loader
@@ -40,6 +58,7 @@ class RegisterForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message="Invalid Email"), Length(min=6, max=30)])
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=20)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=5, max=80)])
+
 
 
 db.create_all()
@@ -76,7 +95,7 @@ def signup():
         new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
-        return 'new user has been created bro!'
+        return redirect(url_for('login'))
 
     return render_template('signup.html', form=form)
 
@@ -84,7 +103,9 @@ def signup():
 @app.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('dashboard.html', name=current_user.username)
+    g.user = current_user.get_id()
+    characters = Character.query.filter(Character.user_id == g.user)
+    return render_template('dashboard.html', name=current_user.username, characters=characters)
 
 
 @app.route('/logout')
@@ -92,6 +113,42 @@ def dashboard():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/character')
+@login_required
+def character():
+    pass
+
+
+@app.route('/characterForm', methods=("GET", "POST"))
+@login_required
+def create_character():
+    pass
+
+
+@app.route('/characterForm/edit/<id>', methods=("GET", "POST"))
+@login_required
+def edit_character(id):
+    pass
+
+
+@app.route('/entry')
+@login_required
+def entry():
+    pass
+
+
+@app.route('/entryForm', methods=("GET", "POST"))
+@login_required
+def create_entry():
+    pass
+
+
+@app.route('/entryForm/edit/<id>', methods=("GET", "POST"))
+@login_required
+def edit_entry(id):
+    pass
 
 
 if __name__ == '__main__':
